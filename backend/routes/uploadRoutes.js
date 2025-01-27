31d2ed77284e43b792c7792857a1f373bde5ecec
Lixetron <dmitry.khlopchenko@proton.me>
2025-01-27 22:25:16 +0300

    fix: fixed download

--- backend/routes/uploadRoutes.js
+++ backend/routes/uploadRoutes.js
@@ -13,56 +13,66 @@ const upload = multer();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-router.post('/resume', upload.single('file'), (req, res) => {
-    const {file} = req;
+router.post(
+    '/resume',
+    upload.single('file'),
+    (req, res) => {
+        const {file} = req;
 
-    if (!['.pdf'].includes(file?.detectedFileExtension)) {
-        res.status(400)
-            .json({
-                message: 'Invalid format',
-            });
-    } else {
-        const filename = `${uuidv4()}${file?.detectedFileExtension}`;
-
-        asyncPipeline(file?.stream, fs.createWriteStream(`${__dirname}/../public/resume/${filename}`))
-            .then(() => {
-                res.send({
-                    message: 'File uploaded successfully', url: `/host/resume/${filename}`,
+        if (!['.pdf'].includes(file?.detectedFileExtension)) {
+            res.status(400)
+                .json({
+                    message: 'Invalid format',
                 });
-            })
-            .catch((err) => {
-                res.status(400)
-                    .json({
-                        message: 'Error while uploading', description: err,
+        } else {
+            const filename = `${uuidv4()}${file?.detectedFileExtension}`;
+
+            asyncPipeline(file?.stream, fs.createWriteStream(`${__dirname}/../public/resume/${filename}`))
+                .then(() => {
+                    res.send({
+                        message: 'File uploaded successfully',
+                        url: `/host/resume/${filename}`,
                     });
-            });
-    }
-});
-
-router.post('/profile', upload.single('file'), (req, res) => {
-    const {file} = req;
-
-    if (!['.jpg', '.png'].includes(file?.detectedFileExtension)) {
-        res.status(400)
-            .json({
-                message: 'Invalid format',
-            });
-    } else {
-        const filename = `${uuidv4()}${file?.detectedFileExtension}`;
-
-        asyncPipeline(file.stream, fs.createWriteStream(`${__dirname}/../public/profile/${filename}`))
-            .then(() => {
-                res.send({
-                    message: 'Profile image uploaded successfully', url: `/host/profile/${filename}`,
+                })
+                .catch((err) => {
+                    res.status(400)
+                        .json({
+                            message: 'Error while uploading',
+                            description: err,
+                        });
                 });
-            })
-            .catch((err) => {
-                res.status(400)
-                    .json({
-                        message: 'Error while uploading', description: err,
+        }
+    });
+
+router.post(
+    '/profile',
+    upload.single('file'),
+    (req, res) => {
+        const {file} = req;
+
+        if (!['.jpg', '.png'].includes(file?.detectedFileExtension)) {
+            res.status(400)
+                .json({
+                    message: 'Invalid format',
+                });
+        } else {
+            const filename = `${uuidv4()}${file?.detectedFileExtension}`;
+
+            asyncPipeline(file.stream, fs.createWriteStream(`${__dirname}/../public/profile/${filename}`))
+                .then(() => {
+                    res.send({
+                        message: 'Profile image uploaded successfully',
+                        url: `/host/profile/${filename}`,
                     });
-            });
-    }
-});
+                })
+                .catch((err) => {
+                    res.status(400)
+                        .json({
+                            message: 'Error while uploading',
+                            description: err,
+                        });
+                });
+        }
+    });
 
 export default router;
